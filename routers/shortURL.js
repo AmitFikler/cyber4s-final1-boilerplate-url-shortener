ddc8413c5d34b0b9fd084e61ca8573405b1b2736
AmitFikler <amitfikler@gmail.com>
2021-11-03 21:03:27 +0200

    added counter

--- routers/shortURL.js
+++ routers/shortURL.js
@@ -3,28 +3,29 @@ const router = express.Router()
 const shortid = require('shortid');
 const fs = require('fs')
 const validUrl = require("valid-url");
+const Database = require("../database/dbClass");
+const { json } = require('body-parser');
 
 
 
 
 
-const urlsObj= {}
 router.post("/api/shorturl/new", (req,res)=>{
         const urlCode = shortid.generate()
-        urlsObj[urlCode] = req.body.longURL
-        let db = JSON.parse(fs.readFileSync('./DB.json',"utf8"))
-        db.push(urlsObj)
-        fs.writeFileSync('./DB.json',JSON.stringify(db))
+        const urlData = new Database(req.body.longURL, urlCode, `http://localhost:3000/${urlCode}`)
+        const save = urlData.saveToDB()
+        if(save) res.send(save)
         res.send(`http://localhost:3000/${urlCode}`)
 })
 
 router.get("/:code", (req,res)=>{
         let code = req.params.code
-        let dbArray = JSON.parse(fs.readFileSync("./DB.json","utf8"))
-        for (let obj of dbArray){
-                if(code in obj){
-                        return res.redirect(301,obj[code])
-                }
+        let dbArray = JSON.parse(fs.readFileSync("./database/DB.json","utf8"))
+        console.log(dbArray)
+        dbArray[code]["count"] += 1
+        fs.writeFileSync("./database/DB.json", JSON.stringify(dbArray))
+        if (code in dbArray){
+                return res.redirect(301,dbArray[code]["longUrl"])
         }
         res.send("no such url")
 })
