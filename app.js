2d9a995f26f50ad43287ed14548dd1faa236be22
AmitFikler <amitfikler@gmail.com>
2021-11-03 13:37:55 +0200

    add webpack to front

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const shortUrl = require("./routers/shortURL")
 const app = express();
 
 app.use(cors());
 
 app.use("/public", express.static(`./public`));
 
+
+
+app.use("/api/shorturl", shortUrl)
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
