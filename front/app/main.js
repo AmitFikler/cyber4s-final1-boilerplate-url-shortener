5d9289498a20a5abcdf3747c546aeab68bd45030
AmitFikler <amitfikler@gmail.com>
2021-11-04 11:57:04 +0200

    Displays the errors to the customer

--- front/app/main.js
+++ front/app/main.js
@@ -6,29 +6,56 @@ const outputURL = document.getElementById("output-url")
 const statisticInput = document.getElementById("statistic-input")
 const statisticBtn = document.getElementById("statisticBtn")
 const statisticOutput = document.getElementById("statistic-output")
+const outputContiner = document.getElementById("output-continer")
 
 
 submitBtn.addEventListener("click", postUrl)
 statisticBtn.addEventListener("click",getStatistic)
 
 async function postUrl() {
-  const response = await axios.post("http://localhost:3000/api/shorturl/new", {
-    "longURL": inputurl.value
-  })
-  outputURL.textContent = response.data
+  try {
+    const response = await axios.post("http://localhost:3000/api/shorturl/new", {
+      "longURL": inputurl.value
+    })
+    
+    outputURL.textContent = response.data
+    outputContiner.style.visibility = "visible"
+  } catch (error) {
+    alartError("Invalid URL") 
+  }
 }
 
 
 async function getStatistic(){
-  const urlCode = statisticInput.value
-  const response = await axios.get(`http://localhost:3000/api/statistic/${urlCode}`)
-  statisticOutput.innerHTML = 
+  try {
+    const urlCode = statisticInput.value
+    const response = await axios.get(`http://localhost:3000/api/statistic/${urlCode}`)
+    statisticOutput.innerHTML = 
+    `
+    Creation Date:  <span id="creationDate">${response.data["creationDate"]}</span> <br><br>
+    Redirect Count:  <span id="redirectCount">${response.data["redirectCount"]}</span> <br><br>
+    Original Url:  <span id="originalUrl">${response.data["originalUrl"]}</span> <br><br>
+    Shorturl Id:  <span id="shorturl-id">${response.data["shorturl-id"]}</span> <br><br>
+    ` 
+  } catch (error) {
+    alartError("No such URL was found in the system") 
+  }
+}
+
+
+function alartError(str){
+  let alart =
   `
-  Creation Date:  <span id="creationDate">${response.data["creationDate"]}</span> <br><br>
-  Redirect Count:  <span id="redirectCount">${response.data["redirectCount"]}</span> <br><br>
-  Original Url:  <span id="originalUrl">${response.data["originalUrl"]}</span> <br><br>
-  Shorturl Id:  <span id="shorturl-id">${response.data["shorturl-id"]}</span> <br><br>
-  `
+  <div class="alert alert-danger" role="alert" id="danger">
+  ${str}
+  </div>`
+  let div = document.createElement("div")
+  div.innerHTML = alart
+  document.body.insertBefore(div, document.querySelector("h1"))
+  setTimeout(()=>{
+      document.getElementById("danger").remove()
+  },2000)
+
 }
 
 
