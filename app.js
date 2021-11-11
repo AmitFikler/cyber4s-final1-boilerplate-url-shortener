29ab2c90f4a9a1373f02222f68f0c6c7314f7226
AmitFikler <amitfikler@gmail.com>
2021-11-11 18:52:39 +0200

    b

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const shortUrl = require("./routers/shortURL")
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const shortUrl = require('./routers/shortURL');
 const app = express();
-const errorHandler = require("./middleware/errorHandler")
+const errorHandler = require('./middleware/errorHandler');
 
 app.use(cors());
 app.use(express.json());
 
-
-app.use("/", express.static(`./front/dist/`));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/front/dist/index.html");
+app.use('/', express.static(`./front/dist/`));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/front/dist/index.html');
 });
 
-app.use("/", shortUrl);
+app.use('/', shortUrl);
 app.use(errorHandler);
 module.exports = app;
